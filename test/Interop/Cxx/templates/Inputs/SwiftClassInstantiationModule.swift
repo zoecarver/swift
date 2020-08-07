69a58ccd89ed3a75b156428431bdfcc0dadedbf3
Marcel Hlopko <hlopko@google.com>
2020-08-07 12:14:27 +0200

    Add test

---
+++ test/Interop/Cxx/templates/Inputs/SwiftClassInstantiationModule.swift
@@ -0,0 +1,6 @@
+import ClassTemplates
+
+public func WOLOLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO() -> MagicWrapper<IntWrapper> {
+  var t = IntWrapper(value: 42)
+  return MagicWrapper<IntWrapper>(t: t)
+}
