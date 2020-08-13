496a30a04d637ff5900e0acd1b37ae96b872cc1d
Marcel Hlopko <hlopko@google.com>
2020-08-13 17:51:58 +0200

    Document failing advanced tests

--- test/Interop/Cxx/templates/Inputs/non-type-parameter.h
+++ test/Interop/Cxx/templates/Inputs/non-type-parameter.h
@@ -2,10 +2,24 @@
 #define TEST_INTEROP_CXX_TEMPLATES_INPUTS_NON_TYPE_PARAMETER_H
 
 template<class T, auto Size>
-struct MagicArray {
-    T t[Size];
+struct TypedeffedMagicArray {
+    T data[Size];
 };
 
-typedef MagicArray<int, 2> MagicIntPair;
+typedef TypedeffedMagicArray<int, 2> MagicIntPair;
+
+template<class T, auto Size>
+struct MagicArray {
+    T data[Size.getValue()];
+};
+
+struct IntWrapper {
+  int value;
+  int getValue() const { return value; }
+};
+
+struct Element {
+    int id;
+};
 
 #endif // TEST_INTEROP_CXX_TEMPLATES_INPUTS_NON_TYPE_PARAMETER_H
