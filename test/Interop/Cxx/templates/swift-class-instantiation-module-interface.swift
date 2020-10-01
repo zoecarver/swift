1ca883bd84f73332e065870ef090bc5f61a39e13
Marcel Hlopko <hlopko@google.com>
2020-10-01 09:37:05 +0200

    Fix

--- test/Interop/Cxx/templates/swift-class-instantiation-module-interface.swift
+++ test/Interop/Cxx/templates/swift-class-instantiation-module-interface.swift
@@ -1,6 +1,5 @@
 // RUN: %empty-directory(%t)
 // RUN: %target-build-swift %S/Inputs/SwiftClassInstantiationModule.swift -module-name SwiftClassInstantiationModule -emit-module -emit-module-path %t/ -I %S/Inputs -Xfrontend -enable-cxx-interop -parse-as-library
-// RUN: tree %t
 // RUN: %target-swift-ide-test -print-module -module-to-print=SwiftClassInstantiationModule -I %t -I %S/Inputs -source-filename=x -enable-cxx-interop | %FileCheck %s
 
 // CHECK: import MagicWrapper
